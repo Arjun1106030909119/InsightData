c8c5ca3987dc769b60dcc9bb32924f65c61f254f
Arjun.V <pallatharjun119@gmail.com>
2026-03-25 12:26:21 +0530

    Initial commit

---
+++ InsightData/src/main/java/com/project/InsightData/InsightDataApplication.java
@@ -0,0 +1,14 @@
+package com.project.InsightData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightDataApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(InsightDataApplication.class, args);
+	}
+
+}
---
+++ InsightData/src/test/java/com/project/InsightData/InsightDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.InsightData;
+
+import org.junit.jupiter.api.Test;
+
+
+class InsightDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
